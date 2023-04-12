2314538ffbbf4366f004608a1c6cff36c14a60bd
pravalikaregu <128493937+pravalikaregu@users.noreply.github.com>
2023-04-12 21:02:54 +0530

    Add files via upload

---
+++ constantUtil.js
@@ -0,0 +1,13 @@
+//Contact Fetch Data
+export const fetchContactColumn = ['Id' , 'Name' , 'Phone'];
+export const dataTableHeaderContactColumn = [{ label: 'Id', fieldName: 'Id' }, { label: 'Name', fieldName: 'Name' }, { label: 'Phone', fieldName: 'Phone' }];
+
+
+//Opprortunity Fetch Data
+export const fetchOpportunityColumn = [ 'Id' , 'Name' , 'Type' , 'Stage']
+export const dataTableHeaderOpportunityColumn = [{ label: 'Id', fieldName: 'Id' }, { label: 'Name', fieldName: 'Name' }, { label: 'Type', fieldName: 'Type' }]
+
+
+//Case Fetch Data
+export const fetchCaseColumn = [] ; 
+export const dataTableHeaderCaseColumn = [] ;
